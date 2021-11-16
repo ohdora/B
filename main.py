9ae4d8db2d69d99fe7fdcedb6cbbee5857eb5955
Daria Nevzorova <67595376+ohdora@users.noreply.github.com>
2021-11-16 14:01:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 server_address = ("", 8000)
 httpd = HTTPServer(server_address, CGIHTTPRequestHandler)
 httpd.serve_forever()
+
+#!/usr/bin/env python3
+
+print("Content-type: text/html")
+print()
+print("<h1>Hello, Daria!</h1>")
